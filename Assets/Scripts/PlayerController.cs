ba1c9675fe2bb6f548ffd1d5ad1bd766a81b9bbc
iballa003 <iballa.montero@gmail.com>
2025-02-10 11:56:26 +0000

    Creado el script PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float moveSpeed;
+    public float moveSpeed = 20f;
     private bool isMoving;
     private Vector2 input;
     void Start()
